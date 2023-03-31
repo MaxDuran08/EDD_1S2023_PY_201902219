0f4a335a665b1e954c908939b887c78db4d82526
MaxDuran08 <2997082920101@ingenieria.usac.edu.gt>
2023-03-31 10:13:53 -0600

    pruebas

--- EDD_Proyecto1_Fase2/index.js
+++ EDD_Proyecto1_Fase2/index.js
@@ -1,1 +1,18 @@
-console.log("hola mama")
+const btn_login=document.getElementById("btn_login")
+//btn_login.addEventListener("click",login)
+
+let loginAdmin = false
+let activeLogin;
+
+function login() {
+    let password =document.getElementById("inputPassword")
+    let user =document.getElementById("inputUser")
+    if(password.value=="admin" && user.value=="admin"){
+        loginAdmin=true
+        activeLogin="admin";
+        window.window.location.replace("./Admin.html")
+    }
+    if(loginAdmin){
+        console.log("Inicio seción como administrador")
+    }
+}
